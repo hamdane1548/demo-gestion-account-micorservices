f30daa78e619f63641111fcd8887d481ccc1ac42
hamdane1548 <oussamahamdane80@gmail.com>
2026-01-27 23:24:20 +0100

    version config in microservice cloud config

--- loans/src/main/java/net/oussama/loans/Dto/AccountEnvSupportDto.java
+++ loans/src/main/java/net/oussama/loans/Dto/AccountEnvSupportDto.java
@@ -5,5 +5,5 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import java.util.List;
 import java.util.Map;
 @ConfigurationProperties(prefix = "contact")
-public record AccountEnvSupportDto(String message, Map<String,String> contact, List<String> support ) {
+public record AccountEnvSupportDto(String message, Map<String,String> contact, List<String> support) {
 }
